40d009888f7833299972d6edf4ba5785ed3aee77
Peter Ogden <Togden013@gmail.com>
2015-12-22 16:34:26 +0000

    Homepage now returns HTML response

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,22 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
+        """ Tests that the homepage can be resolved"""
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        """ Tests that the page is properly constructed in html and contains the
+        expected title"""
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</hmtl>'))
+
