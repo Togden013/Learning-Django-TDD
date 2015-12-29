5ad61c956d5107672782531c9969461a1667d537
Peter Ogden <Togden013@gmail.com>
2015-12-29 18:33:44 +0000

    added functionality to create new list

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -14,6 +14,10 @@ def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
+
     # TODO: Display multiple items in the table
 
     # TODO: Support multiple lists
